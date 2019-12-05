a22c3d1bf7b5e2d44f4022c1a36f2edac3e39f12
Lafont <paullafont@orange.fr>
2019-12-05 21:57:13 +0100

    Deux fonctions d'analyse Html

--- CyberMamieNavigator/DocumentAnalyser.cs
+++ CyberMamieNavigator/DocumentAnalyser.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 using Gecko;
 
@@ -12,9 +13,36 @@ namespace CyberMamieNavigator
         }
 
 
-        public void Analyse(GeckoDocument document)
+        public List<string> Analyse(GeckoDocument document)
+        {
+            List<string> testVocal = new List<string>;
+
+            foreach(GeckoNode node in document.Body.ChildNodes)
+            {
+                if (node.NodeType == NodeType.Element)
+                {
+                    Console.WriteLine(node.NodeName + " " + node.TextContent);
+                    testVocal.Add(node.TextContent);
+                }
+                
+            }
+            return testVocal;
+        }
+
+        public bool BaliseValable(GeckoNode node)
         {
 
+            if (NodeType.Element == node.NodeType)
+            {
+                switch (node.NodeName)
+                {
+                    case "BUTTON":
+                    case "A":
+                        return true;
+                    default: return false;
+                }
+            }
+            return false;
         }
     }
 }
