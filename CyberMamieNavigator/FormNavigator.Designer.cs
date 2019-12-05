a1e22e7c8f5dea72c57fc905d6a320c00baa609a
Yohan <rousseauyohan2@gmail.com>
2019-12-05 19:42:11 +0100

    Ajout html test

--- CyberMamieNavigator/FormNavigator.Designer.cs
+++ CyberMamieNavigator/FormNavigator.Designer.cs
@@ -29,33 +29,22 @@
         private void InitializeComponent()
         {
             this.browser = new Gecko.GeckoWebBrowser();
-            this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // browser
             // 
-            this.browser.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.browser.Location = new System.Drawing.Point(0, 49);
+            this.browser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.browser.Location = new System.Drawing.Point(0, 0);
             this.browser.Name = "browser";
-            this.browser.Size = new System.Drawing.Size(1050, 470);
+            this.browser.Size = new System.Drawing.Size(1050, 519);
             this.browser.TabIndex = 0;
             this.browser.UseHttpActivityObserver = true;
-            // 
-            // panel1
-            // 
-            this.panel1.BackColor = System.Drawing.Color.White;
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1050, 49);
-            this.panel1.TabIndex = 1;
             // 
             // FormNavigator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1050, 519);
-            this.Controls.Add(this.panel1);
             this.Controls.Add(this.browser);
             this.Name = "FormNavigator";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -67,7 +56,6 @@
         #endregion
 
         private Gecko.GeckoWebBrowser browser;
-        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- CyberMamieNavigator/FormNavigator.cs
+++ CyberMamieNavigator/FormNavigator.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.IO;
 using System.Windows.Forms;
 
 using System.Speech.Recognition.SrgsGrammar;
 using System.Speech.Recognition;
+
+using Gecko;
+
 
 namespace CyberMamieNavigator
 {
@@ -12,7 +16,16 @@ namespace CyberMamieNavigator
         {
             InitializeComponent();
 
-            browser.Navigate("http://www.google.fr");
+            browser.LoadHtml(File.ReadAllText("html/test.html"));
+            //browser.Navigate("http://www.google.fr");
+
+            browser.DocumentCompleted += ((se, ea) =>
+            {
+                /*this.Text = this.browser.Document.Title;
+                GeckoElement sc = this.browser.Document.CreateElement("script");
+                sc.SetAttribute("type", "text/javascript");
+                sc.NodeValue = "function sayHello(){alert('hello')}";*/
+            });
         }
     }
 }
