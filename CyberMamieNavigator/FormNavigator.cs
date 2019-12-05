6f15ddd5c5e4202618a0e567e439b6ebafaccc0b
Yohan <rousseauyohan2@gmail.com>
2019-12-05 18:54:26 +0100

    Ajout fichier

---
+++ CyberMamieNavigator/FormNavigator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace CyberMamieNavigator
+{
+    public partial class FormNavigator : Form
+    {
+        public FormNavigator()
+        {
+            InitializeComponent();
+
+            this.geckoWebBrowser1.Navigate("http://www.google.fr");
+        }
+    }
+}
---
+++ CyberMamieNavigator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+
+using Gecko;
+
+namespace CyberMamieNavigator
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+
+            Xpcom.Initialize(Application.StartupPath);
+
+            Application.Run(new FormNavigator());
+        }
+    }
+}
